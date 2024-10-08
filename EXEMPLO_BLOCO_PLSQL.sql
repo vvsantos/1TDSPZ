5ed4b138ea94fe83f5f92610575fc3b6285a4f6a
Vergílio Valério dos Santos <vergilio.santos@hexagon.com>
2024-10-07 22:06:25 -0300

    MEU PRIMEIRO LOOPING

--- EXEMPLO_BLOCO_PLSQL.sql
+++ EXEMPLO_BLOCO_PLSQL.sql
@@ -1,13 +1,28 @@
-CREATE TABLE aula_02 (
-    id   NUMBER
-        GENERATED ALWAYS AS IDENTITY,
-    nome VARCHAR2(30),
-    data DATE
-);
+BLOCO PL/SQL - PROCEDURAL LANGUAGE EM SQL (STRUCTURE QUERY LANGUAGE)
 
+SELECT * FROM aula_02 ORDER BY 1
+DECLARE
+    v_data   DATE := sysdate;
+    v_nome   VARCHAR2(30) := 'MEU NOME';
+    contador NUMBER := 0;
+BEGIN
+    FOR x IN 1..1000 LOOP
+        INSERT INTO aula_02 (
+            nome,
+            data
+        ) VALUES (
+            x
+            || ' '
+            || v_nome,
+            v_data
+        );
 
-SELECT * FROM aula_02;
+        IF MOD(contador, 10) = 0 THEN
+            COMMIT;
+        END IF; 
+        contador := contador + 1;
+    END LOOP;
 
-INSERT INTO AULA_02 (NOME, DATA) VALUES ('PAULO ', SYSDATE +3/24);
-COMMIT;
+    COMMIT;
+END;
 
